c11c310c8f9ab113f3e40924576dae116130bc81
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2024-01-29 20:41:13 +0300

    chore(nvim): fine-tuning the flake8 linter

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -30,7 +30,7 @@ return {
             condition = function()
                 return utils.is_executable("flake8") and not utils.is_executable("pflake8")
             end,
-            prefer_local = ".venv/bin"
+            -- prefer_local = ".venv/bin"
 
         }),
         null_ls.builtins.diagnostics.pyproject_flake8.with({
