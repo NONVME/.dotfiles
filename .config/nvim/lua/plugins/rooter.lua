6c6100f53b990ff1cd0b9c01734ac181453a6ef5
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2024-02-15 19:37:29 +0300

    chore(nvim): rm rooter, whitespace, bufferline

--- .config/nvim/lua/plugins/rooter.lua
+++
@@ -1,5 +0,0 @@
-return {
-    'notjedi/nvim-rooter.lua',
-    enabled = false,
-    config = function() require'nvim-rooter'.setup() end
-}
--- .config/nvim/lua/plugins/vim-better-whitespace.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ntpeters/vim-better-whitespace",
-  config = function()
-    vim.api.nvim_set_hl(0, 'ExtraWhitespace', { bg='#BF616A' })
-    vim.g.current_line_whitespace_disabled_soft = 1
-  end,
-  enabled = false,
-}
