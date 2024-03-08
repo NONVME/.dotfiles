5695e121fb06c24ffdf2827ca2a84e57d733819e
Chashin Efim <69835249+NONVME@users.noreply.github.com>
2024-03-08 23:29:25 +0300

    chore(nvim): fix result selecting

--- .config/nvim/lua/plugins/comlpetions.lua
+++ .config/nvim/lua/plugins/comlpetions.lua
@@ -53,7 +53,17 @@ return {
                         }
                       }
                     }),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+                   ["<CR>"] = cmp.mapping({
+                     i = function(fallback)
+                       if cmp.visible() and cmp.get_active_entry() then
+                         cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+                       else
+                         fallback()
+                       end
+                     end,
+                     s = cmp.mapping.confirm({ select = true }),
+                     c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+                   }),
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
